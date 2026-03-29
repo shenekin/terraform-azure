c0166f6d4e861a68a4f581cf3f02436ece397490
shenekin <281879341@qq.com>
2026-03-29 10:47:26 +0300

    Modify provide in main.tf

--- vm-with-kubernetes/main.tf
+++ vm-with-kubernetes/main.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
 
-provider "azurerm" {
-  features {}
-}
 
 # Resource Group
 resource "azurerm_resource_group" "aks" {
